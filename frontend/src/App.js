007c0eccca691bfa360decff96526e267cf76bfa
ChakitPra19 <chakirpra19@gmail.com>
2025-01-30 23:28:48 +0700

    Add Login Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Login from './Pages/Login';
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Login/>
     </div>
   );
 }
